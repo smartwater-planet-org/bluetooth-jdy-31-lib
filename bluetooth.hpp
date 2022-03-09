cf00460092438252cd5b135c3dbc7f2f5ba56af3
Fernando Vega <fernando.vega@smartwaterplanet.com>
2022-03-09 16:49:41 +0100

    Add base and wrapper classes

--- bluetooth.hpp
+++ bluetooth.hpp
@@ -8,6 +8,105 @@
 #include <SoftwareSerial.h>
 #endif
 
+
+class BT_Base
+{
+    public:
+        virtual ~BT_Base() {}
+
+        virtual void begin(unsigned long baudRate) = 0;
+
+        virtual void begin(unsigned long baudrate, uint16_t config) = 0;
+
+        virtual void end() = 0;
+
+        virtual int available() = 0;
+
+        virtual int availableForWrite() = 0;
+
+        virtual int peek() = 0;
+
+        virtual int read() = 0;
+
+        virtual void flush() = 0;
+
+        virtual size_t readBytesUntil(char terminator, char* buffer, size_t length) = 0;
+
+        virtual size_t readBytes(char* buffer, size_t length) = 0;
+
+        virtual void setTimeout(long timeout) = 0;
+};
+
+template <class C>
+class BT_Wrapper : public BT_Base
+{
+    private:
+        C* m_impl;
+
+    public:
+        BT_Wrapper(C* impl) : m_impl(impl) {}
+
+        ~BT_Wrapper() {}
+
+        virtual void begin(unsigned long baudRate)
+        {
+            m_impl->begin(baudRate);
+        };
+
+        virtual void begin(unsigned long baudrate, uint16_t config)
+        {
+            m_impl->begin(baudrate, config);
+        };
+
+        virtual void end()
+        {
+            m_impl->end();
+        };
+
+        virtual int available()
+        {
+            return m_impl->available();
+        };
+
+        virtual int availableForWrite()
+        {
+            return m_impl->availableForWrite();
+        };
+
+        virtual int peek()
+        {
+            return m_impl->peek();
+        };
+
+        virtual int read()
+        {
+            return m_impl->read();
+        };
+
+        virtual void flush()
+        {
+            m_impl->flush();
+        };
+
+        virtual size_t readBytesUntil(char terminator, char* buffer, size_t length)
+        {
+            return m_impl->readBytesUntil(terminator, buffer, length);
+        };
+
+        virtual size_t readBytes(char* buffer, size_t length)
+        {
+            return m_impl->readBytes(buffer, length);
+        };
+
+        virtual void setTimeout(long timeout)
+        {
+            m_impl->setTimeout(long timeout);
+        };
+
+    private:
+        C* m_impl;
+};
+
 #ifdef SoftwareSerial_h
 class Bluetooth : public SoftwareSerial
 {
